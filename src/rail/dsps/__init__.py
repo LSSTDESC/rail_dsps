1ec7d5feb3f8008f4cbbc8a150f77b0e505c5777
Luca Tortorelli <Luca.Tortorelli@dhcp-visitor-enr-113-83.slac.stanford.edu>
2023-08-01 12:19:25 -0700

    tricking fsps into thinking it is installed

--- src/rail/dsps/__init__.py
+++ src/rail/dsps/__init__.py
@@ -2,7 +2,7 @@ import os
 try:
     os.environ['SPS_HOME']
 except KeyError:
-    os.environ['SPS_HOME'] = os.getcwd()
+    os.environ['SPS_HOME'] = '../examples_data/creation_data/data'
 from ._version import __version__
 
 from rail.creation.engines.dsps_photometry_creator import *
