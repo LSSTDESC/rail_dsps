4c1cc17152846bf64599571335bec182aef33a8d
Luca Tortorelli <Luca.Tortorelli@dhcp-visitor-enr-113-83.slac.stanford.edu>
2023-08-01 12:13:31 -0700

    adding check of SPS_HOME environment variable

--- src/rail/dsps/__init__.py
+++ src/rail/dsps/__init__.py
@@ -1,3 +1,8 @@
+import os
+try:
+    os.environ['SPS_HOME']
+except KeyError:
+    os.environ['SPS_HOME'] = os.getcwd()
 from ._version import __version__
 
 from rail.creation.engines.dsps_photometry_creator import *
