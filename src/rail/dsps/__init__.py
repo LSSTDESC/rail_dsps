a8e9deaaafbcb30b8d2b33331d5177b7b747430f
Luca Tortorelli <Luca.Tortorelli@dhcp-visitor-enr-113-83.slac.stanford.edu>
2023-08-01 15:24:59 -0700

    reverting back to previous version of init

--- src/rail/dsps/__init__.py
+++ src/rail/dsps/__init__.py
@@ -1,11 +1,3 @@
-import os
-try:
-    os.environ['SPS_HOME']
-except KeyError:
-    os.makedirs(os.path.join(os.getcwd(), 'data'), exist_ok=True)
-    with open(os.path.join(os.getcwd(), 'data/emlines_info.dat'),'w') as f:
-        f.write('923.148,Ly 923')
-    os.environ['SPS_HOME'] = os.getcwd()
 from ._version import __version__
 
 from rail.creation.engines.dsps_photometry_creator import *
